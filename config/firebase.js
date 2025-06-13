e58e0f854febfa1b00797836ecaa0ee157fc45e6
Ez-Industrial <jesus.escobedo@ez-industrial.com>
2025-06-13 08:57:14 -0500

    sss

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,24 @@
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Buffer } from "buffer";
 
+dotenv.config({ path: path.join(__dirname, ".env") });
+
+console.log("SERVICE_ACCOUNT_KEY:", process.env.SERVICE_ACCOUNT_KEY || "⚠️ Variable no definida");
+
+if (!process.env.SERVICE_ACCOUNT_KEY) {
+  throw new Error("SERVICE_ACCOUNT_KEY no está definida. Revisa el archivo .env y la ruta.");
+}
+
+const decodedKeyStr = Buffer.from(process.env.SERVICE_ACCOUNT_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decodedKeyStr);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("Ubicación esperada del .env:", path.join(__dirname, ".env"));
+console.log("Directorio de ejecución actual:", process.cwd());
 
 const admin = require("firebase-admin");
 
@@ -7,3 +26,5 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://wash-wheels.firebaseio.com"
 });
+
+console.log("Firebase Service Account Key decodificado correctamente")
--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,24 +1,4 @@
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 import admin from "firebase-admin";
-import { Buffer } from "buffer";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.join(__dirname, ".env") });
-
-const decodedKeyStr = Buffer.from(process.env.SERVICE_ACCOUNT_KEY, "base64").toString("utf8");
-const serviceAccount = JSON.parse(decodedKeyStr);
-
-console.log("Directorio de firebaseAdmin.js:", __dirname);
-console.log("Directorio de ejecución (process.cwd()):", process.cwd());
-console.log("SERVICE_ACCOUNT_KEY:", process.env.SERVICE_ACCOUNT_KEY || "⚠️ Variable no definida");
-
-if (!process.env.SERVICE_ACCOUNT_KEY) {
-  throw new Error("SERVICE_ACCOUNT_KEY no está definida. Revisa el archivo .env y la ruta.");
-}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
