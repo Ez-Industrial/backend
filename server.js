c9859ab34b209a418deded5d8cb185c6f2159989
Ez-Industrial <jesus.escobedo@ez-industrial.com>
2025-06-13 13:18:54 -0500

    d

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { db, asignarRol } from "./config/firebaseAdmin.js";const app = express();
 import Joi from "joi";
 import dotenv from "dotenv";
+import { authenticate, authorizeRole } from "./config/auth.js";
 
 dotenv.config();      
 app.use(express.json());
